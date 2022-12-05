4db970e8962c7cc7ba3a1b0c9a993bf1ee61548f
Ilse M <magi3654@gmail.com>
2022-12-04 21:42:20 -0800

    Houston we have an inconvinient can you check pls

---
+++ app.js
@@ -0,0 +1,74 @@
+'use strict'
+
+import {searchHero} from "./heroFetch.js";
+
+
+async function showHero(){
+    const heroes=document.getElementById('heroes');
+    const search=document.getElementById('search').value;
+    const heroesCatalog= await assembleHeroes(search);
+    heroesCatalog.data.recipes.map(hero=>{
+        console.log(hero.name);
+
+        const heroName=document.createElement('p');
+        heroName.style.fontWeight="bold";
+        heroName.style.fontSize="2rem";
+        heroName.textContent=hero.name;
+
+        const imgHero=document.createElement ('img');
+        imgHero.src=hero.data.results.thumbnail;
+        imgHero.alt=hero.name;
+        imgHero.style="width:10rem; height:10rem; objectFit=cover; objectPosittion:center";
+        
+        const id=document.createElement('p');
+        id.style.fontSize="2rem";
+        id.textContent="Id"+hero.id;
+        
+
+        const infoSection=document.createElement("div");
+        infoSection.appendChild(heroName);
+        infoSection.appendChild(id);
+
+        heroes.append(imgHero);
+        heroes.append(infoSection);
+
+        heroes.style.border="1rem solid #f3f4f3";
+        heroes.style.borderRadius="1.5rem"
+
+    }) 
+}
+
+async function assembleHeroes(data){
+    return await searchHero(data.toLowerCase());
+}
+
+async function cleanData(){
+    search.value="";
+    document.getElementById('heroes').innerHTML="";
+    heroes.style.border="1rem";
+
+}
+
+const heroesCatalog=document.getElementById('app-header-search');
+
+const buscar=document.createElement('input');
+buscar.id="search";
+buscar.style="width:100rem;";
+buscar.placeholder="IronMan...";
+
+const button=document.createElement('button');
+button.textContent="Assemble";
+button.onclick=showHero;
+button.style="font-size:1.2rem;width:10%; height:2rem;margin:2rem;border-radius:1.2rem;background-color:#f3f4f3;font-weight:bolder;padding:1.2rem 2rem;";
+
+const clean=document.createElement('button');
+clean.textContent="Swipe";
+clean.id="Swipe";
+clean.onclick=cleanData;
+clean.style="font-size:1.2rem;width:10%; height:2rem;margin:2rem;border-radius:1.2rem;background-color:#f3f4f3;font-weight:bolder;padding:1.2rem 2rem;";
+
+
+heroesCatalog.append(buscar);
+heroesCatalog.append(button);
+heroesCatalog.append(clean);
+
---
+++ heroFetch.js
@@ -0,0 +1,8 @@
+'use strict'
+
+export const heroSearch= async (heroName)=>{
+    const response=await fetch('https://gateway.marvel.com/v1/public/characters?ts=timestamp&apikey=2cb0e5c92ffe810a3112ed1058a034fe&hash=acf7dddc477b3e9f78917373a3b119b0'
+    +heroName);
+    const data=await response.json();
+    return data;
+}
